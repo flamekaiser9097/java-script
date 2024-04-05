8e90c7787d0a082823cd1dc01398cd6e5bd76459
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2024-04-05 21:10:51 +0530

    Add files via upload

---
+++ Random number generator/random.js
@@ -0,0 +1,14 @@
+// const max=50;
+// const min=100;
+// let random=Math.floor(Math.random()*(max-min))+min;
+// console.log(random);
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min=1;
+const max=6;
+let randomNumber;
+ 
+myButton.onclick=function() {
+    randomNumber=Math.floor(Math.random()*max)+min;
+    myLabel.textContent=randomNumber;
+}
