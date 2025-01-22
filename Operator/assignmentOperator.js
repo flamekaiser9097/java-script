60db2641152d03a9fb28106334b262af19b8c5fe
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-22 19:34:09 +0530

    Add files via upload

---
+++ Operator/assignmentOperator.js
@@ -0,0 +1,22 @@
+// Assignment Operator
+let a=10;
+let b=4;
+console.log('a = ',a,'b = ',b);
+
+a +=4 //a=a+4
+console.log('a+=4',a);
+
+a -=3 //a=a-3
+console.log('a-=3',a);
+
+a *=3 //a=a*3
+console.log('a*=3',a);
+
+a /=3 //a=a/3
+console.log('a/=3',a);
+
+a %=3 //a=a%3
+console.log('a%=3',a);
+
+a **=3 //a=a**3
+console.log('a**=3',a);
