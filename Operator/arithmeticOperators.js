e4dca9084aa09210e6db332daf26deff59f355cf
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-22 19:33:52 +0530

    Add files via upload

---
+++ Operator/arithmeticOperators.js
@@ -0,0 +1,36 @@
+//Arithmetic operators
+let a=10;
+let b=3;
+console.log('a = ',a,'b = ',b);
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+//modulus operator which give us remainder
+console.log("a%/b = ",a%b);
+//Exponentiation operator which give us power
+console.log("a**b = ",a**b);//10^3
+//All the above Arithmetic operator Needs two Operands
+
+
+/*There are some Arithmetic operator which needs only single operands know as unary operator*/
+console.log('a = ',a,'b = ',b);
+
+//Increment operator 
+//a++ means post increment which means first it will print  value than it increment than value
+console.log('a++ = ',a++);
+
+console.log('a = ',a);
+
+//++a means pre increment which means first it will increment the value than print it
+console.log('++a = ',++a);
+
+
+//Decrement operator 
+console.log('b-- = ',b--);
+//b-- means post decrement which means first it will print  value than it decrement than value
+
+console.log('b = ',b);
+
+console.log('--b = ',--b);
+//--b means pre decrement which means first it will decrement the value than print it
