3a66cbb643ba9f0d9238e9a26608459fbee6fbf2
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-23 19:49:19 +0530

    Add files via upload

---
+++ Operator/comparisonOperator.js
@@ -0,0 +1,27 @@
+let a=10;
+let b=3;
+console.log('a = ',a,'b = ',b);
+
+// ==Equal to; 
+console.log('a==b', a==b);
+
+// !=Not Equal to; 
+console.log('a!=b', a!=b);
+
+// === Equal to and type; 
+console.log('a===b', a===b);
+
+// !== Not Equal to and type;
+console.log('a!==b', a!==b);
+
+// > Greater than;
+console.log('a>b', a>b);
+
+//  >= Greater than Equal to;
+console.log('a>=b', a>=b);
+
+//  < Less than;
+console.log('a<b', a<b);
+
+//  <=Less than Equal to;
+console.log('a<=b', a<=b);
