bd502907aa115dba87afa3031e6ce955e6057c03
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-23 19:49:38 +0530

    Add files via upload

---
+++ Operator/logicalOperator.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=3;
+let cond1=a>b;
+let cond2=a===10;
+
+//LogicalAND &&
+console.log('cond1 && cond2 = ',cond1 && cond2);
+
+//LogicalOR ||
+console.log('cond1 && cond2 = ',cond1 || cond2);
+
+//LogicalNOT !
+console.log('!(a<b) = ',!(a<b));
