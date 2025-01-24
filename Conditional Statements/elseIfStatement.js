3ac6cbd5a931f307b63a681a138fddcd5063fba8
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-24 19:13:36 +0530

    Add files via upload

---
+++ Conditional Statements/elseIfStatement.js
@@ -0,0 +1,15 @@
+//else-if statement
+
+//example-1
+let mode='dark';
+let color;
+if(mode==='dark'){
+    color='black';
+}else if(mode==='blue'){
+    color='blue';
+}else if(mode==='pink'){
+    color='pink';
+}else{
+    color='white';
+}
+console.log(color,'color');
