605670dde761721e7b3c6036064c9b2a3943efe7
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-24 19:13:19 +0530

    Add files via upload

---
+++ Conditional Statements/ifElseStatement.js
@@ -0,0 +1,26 @@
+//if else statement
+//example-1
+let mode='dark-mode'
+let color;
+if(mode==='dark-mode'){
+    color='black';
+}else{
+    color='white'
+}
+console.log(color);
+
+//example-2
+let age=24
+if(age>=18){
+    console.log('Eligible to vote');
+}else{
+    console.log('not Eligible to vote');
+}
+
+//example-3
+let no=23
+if(no%2===0){
+    console.log(no,'is even');
+}else{
+    console.log(no,'is odd');
+}
