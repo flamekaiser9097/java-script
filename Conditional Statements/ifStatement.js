bdd730a5243adb33a6d6e623b7796c9de8d2a39a
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-24 19:12:58 +0530

    Add files via upload

---
+++ Conditional Statements/ifStatement.js
@@ -0,0 +1,20 @@
+//if statement
+//example-1
+let age=18;
+if(age>=18){
+    console.log('You can VOTE');
+}
+if(age<18){
+    console.log('You can not VOTE');
+}
+
+//example-2
+let mode='dark';
+let color;
+if(mode==='dark'){
+    color='black';
+}
+if(mode==='light'){
+    color='white';
+}
+console.log(color);
