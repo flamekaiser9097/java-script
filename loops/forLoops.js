aa2f1a985a7712c5d841dda01226a0c15f3c40c0
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-25 20:50:30 +0530

    Add files via upload

---
+++ loops/forLoops.js
@@ -0,0 +1,15 @@
+//for Loop
+
+//execte 5 times
+for(let i=0;i<5;i++){
+    console.log('I love coding');
+}
+console.log('The end');
+
+//calculate sum of 1 to 5
+let sum=0;
+let n=5;
+for(let i=1;i<=n;i++){
+    sum=sum+i;
+}
+console.log('sum=',sum);
