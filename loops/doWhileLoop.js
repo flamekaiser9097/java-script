0cebe64f80529bbe16e9836430875d0dcc3f594d
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-25 20:49:42 +0530

    Add files via upload

---
+++ loops/doWhileLoop.js
@@ -0,0 +1,20 @@
+// do while loop
+//it run atleast 1 time
+
+// do{
+//     do some work
+// }while(condition);
+
+let i=20;
+do{
+    console.log('I love java script');
+    i++;
+}while(i<=10);
+
+//print no from 1 to 5
+
+let j=1;
+do{
+    console.log('j =', j);
+    j++;
+}while(j<=5);
