37b346a3b1406d84be39d43773474439d300f42d
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-01-26 18:41:10 +0530

    Add files via upload

---
+++ loops/forOfLoop.js
@@ -0,0 +1,18 @@
+//for of loop
+
+//help in loop in string and array
+
+let str="Sanjay"
+for( let value of str){//iterator
+    console.log(value);
+}
+
+// calculate length of string
+
+let str1="Mundari"
+let size=0;
+for( let value of str1){//iterator
+    console.log(value);
+    size++;
+}
+console.log('string size =', size);
