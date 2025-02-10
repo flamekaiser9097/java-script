8e6e0ddd4eadf3bce3b320e43f1ab6cc78d797d6
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-02-10 19:33:36 +0530

    Add files via upload

---
+++ string/stringmethod.js
@@ -0,0 +1,16 @@
+//str.toupperCase()   ||convert whole string into upper case
+ let str=` Sanjay Kumar`
+ let str2=` Mundari  `
+ console.log(str.toUpperCase());
+ //str.toupperCase()  ||convert whole string into lower case
+ console.log(str.toLowerCase());
+ //str.trim()  ||remove blank space from back and end
+ console.log(str.trim());
+ //str.slice(start,end?)|| return part of the string
+ console.log(str.slice(1,4));
+ //str.concat(str2) || combine two strings
+ console.log(str.concat(str2));
+ //str.replace(searchval,newval)
+ console.log(str.replace('Kumar','Singh'));
+ //str.charAt(idx) ||find the charachter at particular index number
+ console.log(str.charAt(9));
