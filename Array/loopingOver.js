632e8ba52488a9cf1d52f988c04d50d1c7abfbf2
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-02-11 22:43:54 +0530

    Add files via upload

---
+++ Array/loopingOver.js
@@ -0,0 +1,9 @@
+let student=['sanjay','arun','ashish','jayant']
+//for loop
+for( let i=0;i<student.length;i++){
+    console.log(student[i]);
+}
+//for-of loop
+for(let el of student){
+    console.log(el.toUpperCase());
+} 
