02d9d8ede382f2749273d937f365c8568e0db754
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-02-11 22:43:39 +0530

    Add files via upload

---
+++ Array/array.js
@@ -0,0 +1,9 @@
+//array are collection of same type of item
+let student=['sanjay','arun','ashish','jayant']
+console.log(student);
+console.log(student.length);//property
+let marks=['95','96','94','98']
+//Array indices
+marks[2]='99'
+console.log(marks);
+console.log(marks[0]);
