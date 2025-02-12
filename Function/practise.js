89b6978a849fc29f2f7f7c5dee395273bb044abb
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-02-12 19:59:12 +0530

    Add files via upload

---
+++ Function/practise.js
@@ -0,0 +1,24 @@
+// Qs. Create a function using the "function" keyword that takes a String as an argument & returns the number of vowels in the string.
+
+function vowels(str){
+    let count=0;
+    for( const char of str){
+         if(char==='a'||char==='e'||char==='i'||char==='o'||char==='u'){
+            count++;
+         }
+    }
+    console.log(count);
+}
+
+
+
+// Qs. Create an arrow function to perform the same task. 
+const countVowel=(str)=>{
+    let count=0;
+    for( const char of str){
+         if(char==='a'||char==='e'||char==='i'||char==='o'||char==='u'){
+            count++;
+         }
+    }
+    console.log(count);
+}
