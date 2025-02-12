e78944c4c10ed64c325e5e3629b638377b6f1151
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-02-12 19:58:18 +0530

    Add files via upload

---
+++ Function/arrowfunction.js
@@ -0,0 +1,23 @@
+//function for doing sum 
+function sum(a,b){
+    return a+b;
+}
+
+// calculating sum using Arrow function =>
+const arrowSum=(a,b)=>{
+console.log(a+b);
+}
+arrowSum(35,78);
+
+
+//function for doing multiplication
+function mul(a,b){
+    return a*b;
+}
+
+// calculating multiplication using Arrow function =>
+    const arrowMul=(a,b)=>{
+       return a*b;
+        }
+        
+// Arrow function =>
