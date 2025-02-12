61d156fc2964ffd19ab7af10646827d5d9df1069
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2025-02-12 19:59:30 +0530

    Add files via upload

---
+++ Function/practise1.js
@@ -0,0 +1,6 @@
+// Qs. For a given array of numbers, print the square of each value using the forEach loop.
+let arr=[2,4,6,8,10]
+arr.forEach((val)=>{
+    // console.log(val*val);
+    console.log(val**2);
+})
